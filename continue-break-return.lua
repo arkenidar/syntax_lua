97b4bc6b3c728d28435e92cb70fc785dfb60c637
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-10-25 14:24:38 +0200

    as labels: continue break return

---
+++ continue-break-return.lua
@@ -0,0 +1,20 @@
+
+local function example1()
+-- function's begin
+  
+-- continue break return: break and return are keywords in Lua 
+
+while true do
+-- loop's begin
+
+-- you may add operations here
+
+--- just before loop's end
+:: continue_label :: -- goto continue_label: continue next loop iteration
+end -- while loop's end
+--- just after loop's end
+:: break_label :: -- goto break_label: break the loop, exit the loop
+
+--- just before function's end
+:: return_label :: -- goto return_label: exit the function thus returning to this function's caller, from calle
+end -- example1 function's end
